56cfd157a38cd7370b198a159c8af9ee8eb5f3c3
Arturo Tenorio <arturo-turris@hotmail.com>
2021-05-22 11:47:38 -0500

    Updating our app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, Switch} from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 import './App.css';
@@ -8,14 +8,8 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component'
-import {auth,createUserProfileDocument} from './firebase/firebase.utils'
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
-
-const HatsPage= () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 class App extends React.Component {
   componentDidMount(){
@@ -49,17 +43,23 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage}/>
-          <Route exact path='/hats' component={HatsPage}/>
-          <Route exact path='/signIn' component={SignInSignUpPage}/>
-          <Route exact path='/shop' component={ShopPage}/>
+          <Route path='/shop' component={ShopPage}/>
+          <Route exact path='/signIn' render={() => this.props.currentUser ? (<Redirect to='/'/>) : (<SignInSignUpPage/>)}/>
         </Switch>
       </div>
     ); 
   }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
