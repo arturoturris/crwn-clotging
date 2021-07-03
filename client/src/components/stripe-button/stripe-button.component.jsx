a136ba21adb630937c3aff622bb119394a9b89d8
Arturo Tenorio <arturo-turris@hotmail.com>
2021-07-03 00:30:23 -0500

    Trying to fix stripe test payments

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51J0ThyHC3byRgcmz8T2N0qH3Zs6Xrcl36DEmH6ts19KAYx6VWbEZVkDKDE8jPebq1XDKatmsKrQRNW1duNQ9B2pl0020r43JXR";
+
+  const onToken = (token) => {
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceForStripe,
+        token,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        alert("Payment successful");
+      })
+      .catch((error) => {
+        console.log("Payment error: ");
+        console.log(error);
+        alert(
+          "There was an issue with your payment. Please make sure you use the provided credit card."
+        );
+      });
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
